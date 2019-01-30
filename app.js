666fc4a3e45a8e31d886a96c5cd954f15afc21a8
nkardia <nkardia@gmail.com>
2019-01-29 21:59:52 -0300

    Challenge 18

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,23 +10,64 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 const app = express();
 
 app.set('view engine', 'ejs');
-
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(express.static("public"));
 
+// VARS
+let posts = [];
 
+// GET
+app.get("/", (req, res) => {
+    res.render("home", {
+        startingContent: homeStartingContent,
+        posts: posts
+    });
+});
 
+app.get("/about", (req, res) => {
+    res.render("about", {
+        startingContent: aboutContent
+    });
+});
 
+app.get("/contact", (req, res) => {
+    res.render("contact", {
+        startingContent: contactContent
+    });
+});
 
+app.get("/compose", (req, res) => {
+    res.render("compose", {});
+});
 
+app.get("/posts/:title", (req, res) => {
+    let isMatch = false;
+    posts.forEach(post => {
+        if (lodash.lowerCase(req.params.title) === lodash.lowerCase(post.title)) {
+            isMatch = true;
+        }
+    });
 
+    if (isMatch) {
+        console.log("It's a match!");
+    } else {
+        console.log("No match. SRY");
+    }
+});
 
+// POST
+app.post("/compose", (req, res) => {
+    const post = {
+        title: req.body.postTitle,
+        body: req.body.postBody
+    };
+    posts.push(post);
+    res.redirect("/");
+});
 
-
-
-
-
-
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+// LISTEN
+app.listen(3000, function () {
+    console.log("Server started on port 3000");
 });
